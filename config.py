3d73a7c09fb98fe7798f2dca4187363381b2006b
Dajirom21 <133806182+Dajirom21@users.noreply.github.com>
2023-08-21 18:59:30 +0200

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-ORIGIN_DATA="data/movimientos.db"
-SECRET_KEY = "Clave"
-apikey = "64DDF6A7-4465-4D7F-8BD7-A761CCC07931"
-cryptos = ('BTC', 'ETH', 'USDT', 'BNB', 'XRP', 'ADA', 'SOL', 'DOT', 'MATIC')
