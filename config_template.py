0720915275b00b6a2b88ad68fae21483c14c53a9
Dajirom21 <133806182+Dajirom21@users.noreply.github.com>
2023-08-21 19:01:25 +0200

    Add files via upload

---
+++ config_template.py
@@ -0,0 +1,4 @@
+ORIGIN_DATA="data/movimientos.db"
+SECRET_KEY = "Obetener secret key"
+apikey = "Poner apikey personal de cada uno en coniapi.io"
+cryptos = ('BTC', 'ETH', 'USDT', 'BNB', 'XRP', 'ADA', 'SOL', 'DOT', 'MATIC')
---
+++ main.py
@@ -0,0 +1,1 @@
+from my_cripto import app
